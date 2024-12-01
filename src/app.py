a4526f748d0803b791ca4d296947b3e95da624cf
hamed <hamed2005@gmail.com>
2024-12-01 15:57:39 +0100

    Remove uncessary imports

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 import joblib
-import numpy as np
 from typing import Literal
 import pandas as pd
 
